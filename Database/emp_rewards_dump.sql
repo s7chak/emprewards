536adc67331b763254dca960b0a33afe7a60bc5a
DhrovS <dhrov1@gmail.com>
2019-11-15 15:02:36 -0600

    DB changes

--- Database/emp_rewards_dump.sql
+++ Database/emp_rewards_dump.sql
@@ -9,46 +9,53 @@ date_added timestamp NOT NULL);
 
 CREATE TABLE emprewardz_point_holder (
   user_id int(11) NOT NULL,
+  total int(10) NOT NULL DEFAULT 0,
   points int(10) NOT NULL,
   month date NOT NULL,
-  month_id int(11) NOT NULL,
-  PRIMARY KEY (user_id,month_id)
+  month_id0 int(11) NOT NULL,
+  PRIMARY KEY (user_id,month_id0)
 );
 
 ALTER TABLE emprewardz_point_holder ADD CONSTRAINT fk_points FOREIGN KEY (user_id) REFERENCES users(pk_user_id);
+ALTER TABLE emprewardz_transact_points ADD CONSTRAINT fk_month1 FOREIGN KEY (month_id0) REFERENCES months(month_id);
+
  
+CREATE TABLE months(
+	month_id int(11) PRIMARY KEY AUTO_INCREMENT
+)
 
 CREATE TABLE emprewardz_transact_points (
   source_user int(11) NOT NULL,
   dest_user int(11) NOT NULL,
   points int(10) NOT NULL,
-  transact_date DATE NOT NULL
+  transact_date DATE NOT NULL,
+  month_id1 int(11) NOT NULL
 );
 
 ALTER TABLE emprewardz_transact_points ADD CONSTRAINT fk_users FOREIGN KEY (source_user) REFERENCES users(pk_user_id);
 ALTER TABLE emprewardz_transact_points ADD CONSTRAINT fk_users2 FOREIGN KEY (dest_user) REFERENCES users(pk_user_id);
+ALTER TABLE emprewardz_transact_points ADD CONSTRAINT fk_month1 FOREIGN KEY (month_id1) REFERENCES months(month_id);
+
 
 CREATE TABLE emprewardz_redemption (
   user_id int(11) NOT NULL,
   points_redeemed int(10),
   date_redeemed DATE NOT NULL,
-  month DATE NOT NULL,
-  PRIMARY KEY (user_id,month)
+  month_id2 int(11) NOT NULL,
+  PRIMARY KEY (user_id,month_id2)
 );
 
 ALTER TABLE emprewardz_redemption ADD CONSTRAINT fk_redeem FOREIGN KEY (user_id) REFERENCES users(pk_user_id);
-
+ALTER TABLE emprewardz_transact_points ADD CONSTRAINT fk_month1 FOREIGN KEY (month_id2) REFERENCES months(month_id);
 
 CREATE or REPLACE View agg_points AS
-Select A.user_id, A.PointsRedeemed, A.PointsGiven, B.PointsRecieved from
-	(SELECT user_id, points_redeemed as PointsRedeemed, points as PointsGiven from emprewardz_redemption as red join emprewardz_transact_points as trpo
+Select A.user_id, A.month_id2, A.PointsRedeemed, A.PointsGiven, B.PointsRecieved from
+	(SELECT user_id, month_id2,  points_redeemed as PointsRedeemed, points as PointsGiven from emprewardz_redemption as red join emprewardz_transact_points as trpo
 	on red.user_id = trpo.source_user
-	group by user_id) AS A Join
-	(SELECT user_id, points_redeemed as PointsRedeemed, points as PointsRecieved from emprewardz_redemption as red join emprewardz_transact_points as trpo
-	on red.user_id = trpo.source_user
-	group by user_id) AS B
-	on A.user_id=B.user_id;
-
+	group by month_id2,user_id) A Left Join
+	(SELECT dest_user, month_id1, points as PointsRecieved from emprewardz_transact_points
+	group by month_id1,dest_user) B
+	on A.user_id=B.dest_user and A.month_id2=B.month_id1;
 	
 DELIMITER //
 CREATE PROCEDURE stored_proc(
@@ -65,7 +72,7 @@ BEGIN
     ORDER BY month_id DESC LIMIT 1; 
 	
 	UPDATE emprewardz_point_holder
-    SET emprewardz_point_holder.points = emprewardz_point_holder.points + pointsGR 
+    SET emprewardz_point_holder.points = emprewardz_point_holder.total + pointsGR 
 	WHERE dest = emprewardz_point_holder.user_id
     ORDER BY month_id DESC LIMIT 1;
 END//
